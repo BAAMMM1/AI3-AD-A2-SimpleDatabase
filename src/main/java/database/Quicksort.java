20077e37c49069b76a8294d1b7e7bcfc534b9245
abw012 <dennis.eickholt@haw-hamburg.de>
2018-05-27 14:24:14 +0200

    Quicksort another try

--- src/main/java/database/Quicksort.java
+++ src/main/java/database/Quicksort.java
@@ -2,117 +2,73 @@ package database;
 
 import database.filter.FilterType;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author Shadai on 26.05.2018
+ * @author Shadai on 27.05.2018
  */
 public class Quicksort {
-    private ArrayList<City> result = new ArrayList<City>();
-    private City pivot;
-    private City left;
-    private City right;
-
+    private FilterType filter;
+    private List<City> sortedList;
 
     public List<City> sort(List<City> cities, FilterType filter) {
-        ArrayList<City> result = new ArrayList<City>();
-        ArrayList<Integer> lockedList = new ArrayList<Integer>();
 
-        int length = result.size();
+        //Initialize
+        this.filter = filter;
+        sortedList = cities;
 
-        do {
-            //10.   Bestimme Bereich, der geprüft werden muss
-
-
-            if (lockedList.size()==0) {
-                // 1. Initialisierung mit ersten Werten
-                // pivot bilden -> Median fällt weg, weil wir hier Liste sortieren wollen.
-                // daher einfach ersten wert als pivot
-                pivot = result.get(0);
-                left = result.get(1);
-                right = result.get(result.size() - 1);
-            }
-            else{
-                //10a Nimm erstes Element aus locked list, welches
-                //nicht an seine richtigen Position ist
-                int i = 0;
-
-                while (result.indexOf(result.get(i))==lockedList.indexOf(i)){i++;}
-                pivot = result.get(i);
-                left = result.get(i+1);
-
-                i = i+2;
-                while (!(result.indexOf(result.get(i))==lockedList.indexOf(i))&&i<result.size()){i++;}
-                right = result.get(i);  // bis nächste fixierung oder bis Ende
-            }
-
-            //5. Mach das so lange, bis left größer right ist
-
-            do {
-                //2.Left index setzen
-                //  Left geht so lange mit dem Index hoch, bis der wert, der sich hinter dem index befindet,
-                //  größer dem pivot
-                while (left.compareTo(pivot, filter) == -1) {
-                    left = result.get(result.indexOf(left) + 1);
-                }
-
-                //3.Rechts index setzen
-                //  Rechts geht so lange mit dem Index runter, bis der Wert kleiner ist als der pivot
-                while (right.compareTo(pivot, filter) == 1) {
-                    right = result.get(result.indexOf(right) + 1);
-                }
-
-                //4. Tausche left mit right
-                swap(result, left, right);
-            }
-            while (result.indexOf(left) < result.indexOf(right)); //5. So lange bis Left und Right vertauscht sind (Right - Left)
-
-            //6. Right Position ist die fixe Position von pivot
-            swap(result, right, pivot);
-
-            //7. Fixierte Position in Liste zwischenspeichern
-            lockedList.add(result.indexOf(pivot));
-
-            //8. Prüfe, ob ungeprüfte Bereiche nun alleine stehen, falls ja, setz sie auf locked
-            proof(lockedList);
-        } while (!(result.size()==lockedList.size()));  //9. wiederhole den Schritt, bis alle in der Liste locked sind
+        //1. Sortiere Liste
+        sortWithQuickSort(sortedList);
 
 
+        return sortedList;
+    }
 
-        return result;
+    private void sortWithQuickSort(List<City> toSortList) {
+        //2. Wie sortWithQuickSort ich? in dem ich quick sort von index 0 bis Ende anwende
+        qSort(toSortList, 0, toSortList.size() - 1);
+    }
+
+    private void qSort(List<City> cities, int left, int right) {
+        if (left < right) {
+            int i = gettingFixedIndex(cities, left, right); //um festzustellen, ob es einen kleineren Part gibt, der erst sortiert werden muss
+            //qSort nun linken Teil
+            qSort(sortedList, left, i - 1);
+            //qSort dann rechten Teil
+            qSort(sortedList, i + 1, right);
+        }
 
     }
 
-    private void proof(ArrayList<Integer> lockedList) {
-        //hinzugefügte Zahl ist immer an letzter Position, diese nun
-        //hier an richtige Position bringen
-        if (lockedList.size()>1){
-            for (int i = 0;i <lockedList.size(); i++){
-               if (lockedList.get(lockedList.size()-1)<i){
-                   lockedList.add(i,lockedList.size()-1);
-                   lockedList.remove(lockedList.size()-1);
-                }
-            }
+
+    //statt mit werten hinter indexen -> hier statt Arralist<Intger> i[] -> i[4] = irgendein Int wert zu arbeiten
+    // muss nun mit Arralist<City> cities -> City[] cities
+
+    //cities[index].getValue(filterType) um an die werte hinter z.b. PLZ zu kommen
+
+    private int gettingFixedIndex(List<City> cities, int left, int right) {
+        // 1. Pivot setzen, hier 1. Element
+        City pivot = cities.get(right);
+        City leftCity = cities.get(left);
+        City rightCity = cities.get(right - 1);
+        while (cities.indexOf(leftCity) <= cities.indexOf(rightCity)) {
+            //if (leftCity.compareTo(rightCity, filter) == 1) {
+            if (leftCity.getValue(filter) > pivot.getValue(filter)) {
+                //tausche cities left und right
+                System.out.println("rightcityIndex vor swap = "+cities.indexOf(rightCity));
+                System.out.println("leftcityIndex vor swap = "+cities.indexOf(leftCity)+"\n");
+                swap(cities, leftCity, rightCity);
+
+                System.out.println("rightcityIndex = "+cities.indexOf(rightCity));
+                System.out.println("leftcityIndex = "+cities.indexOf(leftCity)+"\n---------------------------\n");
+                rightCity = cities.get(cities.indexOf(rightCity) - 1);
+
+            } else leftCity = cities.get(cities.indexOf(leftCity) + 1);
         }
-        //Wenn Liste nur 1 oder 2 Elemente hat, kann
-        //da kein Element dazwischen gefüllt werden
-        if (!(lockedList.size()<3)) {
+        swap(cities, leftCity, cities.get(right));
 
-            // Liste muss von 0 - result length gehen,
-            // falls nun [0,1,3,4], dann muss 2 ebenfalls fixiert sein
 
-            for (int i = lockedList.get(0); i <= lockedList.size() - 2; i++) {
-                // Wenn der nächste Index nach i 2 größer ist als i
-                if (lockedList.get(i + 1) == lockedList.get(i) + 2) ;
-                lockedList.add(lockedList.get(i) + 1);
-            }
-
-            //Falls nur noch der Letzte Index fehlt bei der Fixierung, fixiere ihn
-            if (lockedList.size() == result.size() - 1) {
-                lockedList.add(result.size() - 1);
-            }
-        }
+        return cities.indexOf(leftCity);
 
     }
 
