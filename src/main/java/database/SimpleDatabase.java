7de1951c08a8e54c791a48bea83b4265744df963
abr227 <christian.graumann@haw-hamburg.de>
2018-05-27 15:53:40 +0200

    polish

--- src/main/java/database/SimpleDatabase.java
+++ src/main/java/database/SimpleDatabase.java
@@ -1,44 +1,81 @@
 package database;
 
 import database.filter.Filter;
-import database.filter.FilterType;
 import database.searching.BinarySearch;
 import database.sorting.SelectionSort;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /**
+ * Diese Klasse stellt eine einfache Datenbank von City-Objekten auf Basis einer CSV-Datei da.
+ *
  * @author Chris on 08.05.2018
  */
 public class SimpleDatabase {
 
+    /**
+     * Gibt an in welchem Charset die CSV-Datei erstellt wurde
+     */
     private static final String CHARSET = "ISO-8859-1";
 
+    /**
+     * Trennzeichen der CSV-Spalten
+     */
     private static final String SYMBOL_DELIMITER = ";";
+
+    /**
+     * Symbol in der CSV-Datei für Spalten die keinen Wert enthalten
+     */
     private static final String SYMBOL_DEFAULT_VALUE = "/";
+
+    /**
+     * Dezimalsymbol Englisch
+     */
     private static final String SYMBOL_DECIMAL_ENGLISH = ",";
+
+    /**
+     * Dezimalsymbol Deutsch
+     */
     private static final String SYMBOL_DECIMAL_GERMAN = ".";
+
+    /**
+     * Spacesymbol der CSV-Datei
+     */
     private static final String SYMBOL_SPACE = " ";
+
+    /**
+     * Symbol für einen leeren String
+     */
     private static final String SYMBOL_EMPTY = "";
 
+    /**
+     * Default-Wert für die City-Objekte für Spalten in der CSV-Datei die keinen Wert besitzen
+     */
     private static final String DEFAULT_VALUE = "0";
 
+    /**
+     * Default - Konstruktor
+     */
     public SimpleDatabase() {
+
     }
 
     /**
-     * Diese Methode stellt das Einlesen von Städten eines Abschnitts einer übergebenen CSV-Datei von Start-Zeile
-     * bis End-Zeile da. Die CSV-Datei dient dabei als simple Datenbank. Eine Zeile der CSV-Datei stellt dabei ein
-     * Objekt der Klasse City da.
+     * Diese Methode stellt das Einlesen von Städten eines Abschnitts einer übergebenen CSV-Datei von Start-Zeile bis
+     * End-Zeile da. Die CSV-Datei dient dabei als simple Datenbank. Eine Zeile der CSV-Datei stellt dabei ein Objekt
+     * der Klasse City da.
      *
-     * @param path  Pfad der CSV-Datei
+     * @param path  Pfad der CSV-Datei, darf nicht leer sein
      * @param start start > 0
      * @param end   end >= start
      * @return falls Datei nicht gefunden oder Fehler beim einlesen eine leere Liste, sonst Cities von Start bis End
-     * @throws IOException
+     * @throws IOException falls Pfad oder Charset unbekannt und fehler beim einlesen der CSV-Datei
      */
     public List<City> load(String path, int start, int end) {
 
@@ -63,7 +100,6 @@ public class SimpleDatabase {
 
                 // 2.1 Lese jede Zeile ein
                 while ((line = reader.readLine()) != null) {
-
                     // 2.2 Prüfe ob die Zeile im Intervall liegt.
                     index += 1;
                     if (index < start) continue;
@@ -108,7 +144,7 @@ public class SimpleDatabase {
      * @return falls kein Symbol Space vorhanden dann keine Veränderung
      */
     private String replaceSpace(String string) {
-        if(string == null) throw new IllegalArgumentException();
+        if (string == null) throw new IllegalArgumentException();
 
         return string.replace(SYMBOL_SPACE, SYMBOL_EMPTY).replace(SYMBOL_DEFAULT_VALUE, DEFAULT_VALUE);
     }
@@ -118,10 +154,10 @@ public class SimpleDatabase {
      *
      * @param string darf nicht null sein und muss einen Integer-Wert darstellen
      * @return Integer-Wert des Strings
-     * @exception NumberFormatException falls der String keinen Integer-Wert darstellt
+     * @throws NumberFormatException falls der String keinen Integer-Wert darstellt
      */
     private Integer parseIntValue(String string) {
-        if(string == null) throw new IllegalArgumentException();
+        if (string == null) throw new IllegalArgumentException();
 
         return Integer.valueOf(string);
     }
@@ -131,16 +167,27 @@ public class SimpleDatabase {
      *
      * @param string darf nicht null sein und muss einen Double-Wert darstellen
      * @return Double-Wert des Strings
-     * @exception NumberFormatException falls der String keinen Double-Wert darstellt
+     * @throws NumberFormatException falls der String keinen Double-Wert darstellt
      */
     private Double parseDoubleValue(String string) {
-        if(string == null) throw new IllegalArgumentException();
+        if (string == null) throw new IllegalArgumentException();
 
         return Double.valueOf(string.replace(SYMBOL_DECIMAL_ENGLISH, SYMBOL_DECIMAL_GERMAN));
     }
 
 
+    /**
+     * Die Methode find liefert eine Liste von City-Objekten aus der übergebenen Liste an City-Objekten zurück,
+     * die alle Kritierien der Filerliste erfüllen.
+     *
+     * @param filterList darf nicht null sein, falls leer wird kein Filter angewendet
+     * @param database   darf nicht null sein, falls leer wird eine leere Liste zurück gegeben
+     * @return gefilterte Liste
+     */
     public List<City> find(List<Filter> filterList, List<City> database) {
+
+        if (filterList == null) throw new NullPointerException("filter list must not be null");
+        if (database == null) throw new NullPointerException("database must not be null");
 
         // 1. Ergebnisliste = alles
         List<City> result = database;
